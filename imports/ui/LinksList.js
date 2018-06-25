ec510433966300f62f4de9bae704f13802fa1046
Christriane <Christriane.marquez@gmail.com>
2018-06-25 20:28:17 +0000

    Style items, empty items, filters, page content and private header

--- imports/ui/LinksList.js
+++ imports/ui/LinksList.js
@@ -29,6 +29,14 @@ export default class LinksList extends React.Component {
     }
 
     renderLinksListItems(){
+        if(this.state.links.length === 0){
+            return (
+                <div className="item">
+                    <p className="item__status-message">No Links found.</p>
+                </div>
+            );
+        }
+        
         return this.state.links.map((link) => {
             const shortUrl = Meteor.absoluteUrl(link._id);
             return <LinksListItem key={link._id} shortUrl={shortUrl} {...link}/>
@@ -38,7 +46,6 @@ export default class LinksList extends React.Component {
     render(){
         return(
             <div>
-                <p>Links List</p>
                 <div>
                     {this.renderLinksListItems()}
                 </div>
